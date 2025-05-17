d81d83bedf3120809ecb9d1bdab7a5ee60dfa3be
LOFRZ <almenzo.arroyo@gmail.com>
2025-05-18 00:42:38 +0200

    up

--- app/components/navbar/SearchFilters.tsx
+++ app/components/navbar/SearchFilters.tsx
@@ -1,6 +1,13 @@
 const SearchFilters = ()  => {
     return (
-        <>testmed</>
+        <div className="h-[64px] flex flex-row items-center justify-between border rounded-full">
+            <div className="flex flex-row items-center justify-between">
+                <div className="w-[250px] h-[64px] px-8 flex justify-center flex-col rounded-full hover:bg-gray-100">
+                    <p className="text-xs font-semibold">Where?</p>
+                    <p className="text-sm">Where doc?</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
