e5c01dfdc8f4853b7db8dbbbbc7b7761f5c59510
LOFRZ <almenzo.arroyo@gmail.com>
2025-05-19 18:09:14 +0200

    color fix

--- app/components/navbar/SearchFilters.tsx
+++ app/components/navbar/SearchFilters.tsx
@@ -19,7 +19,7 @@ const SearchFilters = ()  => {
 
                 
                 <div className="p-2">
-                <div className="cursor-pointer p-2 lg:p-4 bg-airbnb hover:bg-airbnb-dark transition rounded-full text-white">
+                <div className="cursor-pointer p-2 lg:p-4 bg-medaide hover:bg-medaide-dark transition rounded-full text-white">
                     <svg 
                         viewBox="0 0 32 32" 
                         style={{display:'block', fill:'none', height: '16px', width: '16px', stroke: 'currentColor', strokeWidth:4, overflow:'visible'}} 
