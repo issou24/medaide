2d71805cc8244d83ce46c1417354b48fdfb628b1
LOFRZ <almenzo.arroyo@gmail.com>
2025-05-20 00:17:55 +0200

    depot

--- app/components/application/ReservationSidebar.tsx
+++ app/components/application/ReservationSidebar.tsx
@@ -4,11 +4,24 @@ const ReservationSidebar = () => {
             <h2 className="mb-5 text-2xl">ttttt</h2>
 
             <div className="mb-6 p-3 border border-gray-400 rounded-xl">
-                <label className="block font-bold text-xs">info</label>
+                <label className="mb-2 block font-bold text-xs">info</label>
+
                 <select className="w-full -ml-1 text-xm">
                     <option>A</option>
                     <option>B</option>
                 </select>
+            </div>
+
+            <div className="w-full mb-6 py-6 text-center text-white bg-medaide hover:bg-medaide-dark rounded-xl">Valider la candidature</div>
+
+            <div className="mb-4 flex justify-between align-center">
+                <p>  va sauter</p>
+                <p> peut etre validation</p>
+            </div>
+
+            <div className="mt-4 flex justify-between align-center font-bold">
+                <p>  paye</p>
+                <p> ho vey ho vey goy</p>
             </div>
         </aside>
     )
