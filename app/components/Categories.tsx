2ec40a451b4be35a45d43bf8938526ffaa987e29
LOFRZ <almenzo.arroyo@gmail.com>
2025-05-19 21:05:15 +0200

    icone

--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,8 +1,15 @@
+import Image from 'next/image';
+
 const Categories = () => {
     return  (
         <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
             <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:opacity-100">
-                test
+                <Image
+                        src="/icn_category_beach.jpeg"
+                        alt="Category - Beach"
+                        width={20}
+                        height={20}
+                />
             </div>
         </div>
     )
