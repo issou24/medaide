ee6f425f1a3dccc3ba12dd3c4e839773d0eda612
LOFRZ <almenzo.arroyo@gmail.com>
2025-05-21 22:08:45 +0200

    inbox

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,11 @@
+const Conversation = () => {
+    return (
+        <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
+            <p className="mb-6 text-xl">NOM MED</p>
+
+            <p className="text-medaide-dark">go conv </p>
+        </div>
+    )
+}
+
+export default Conversation;
--- app/myoffers/page.tsx
+++ app/myoffers/page.tsx
@@ -6,6 +6,10 @@ const MyOffers= () => {
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
             <h1 className="mt-6 mb-2 text-2xl">offre</h1>
+
+            <div className="grid grid-cols-1 md:grid-cols-3 gzp->6">
+                <ApplicationList />
+            </div>
         </main>
     )
 }
