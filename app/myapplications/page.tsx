deb317e80c159696313bf8a7f19dc3fa5227c0b5
LOFRZ <almenzo.arroyo@gmail.com>
2025-05-21 17:23:23 +0200

    page up

--- app/myapplications/page.tsx
+++ app/myapplications/page.tsx
@@ -5,7 +5,28 @@ import ContactButton from "@/app/ContactButton";
 const MyApplicationsPage = () => {
     return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
-            my appli
+            <h1 className="mt-6 mb-2 text-2xl">Med app</h1>
+
+            <div className="pt-6 pb-2">
+                <div className="p-5 mt-4 grid grid-cols-4 gap-4 shadow-md border border-gray-300">
+                    <div className="col-span-1">
+                        <div className="relative overflow-hidden aspect-square rounded-xl">
+                            <Image
+                                fill
+                                src="/beach_1.jpg"
+                                className="hover:scale-110 object-cover transition h-full w-full"
+                                alt="applications"
+                            />    
+                        </div>
+                    </div>
+
+                    <div className="col-span-3 space-y-2">
+                        <h2 className="mb-4 text-xl">
+                            detail appli
+                        </h2>
+                    </div>
+                </div>
+            </div>
         </main>
     )
 }
