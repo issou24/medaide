27e4e5453ea931e8cc1629b0f898e60250f00fd7
LOFRZ <almenzo.arroyo@gmail.com>
2025-05-22 23:12:11 +0200

    ^^

--- app/components/inbox/ConversationDetail.tsx
+++ app/components/inbox/ConversationDetail.tsx
@@ -1,8 +1,30 @@
 const ConversationDetail = () => {
     return (
-        <div>
-            dfghjk
-        </div>
+        <>
+            <div className="max-h-[400px] overflow-auto flex flex-col space-y-4">
+                <div className="w-[80%] py-4 px-6 rounded-xl bg-gray-200">
+                    <p className="font-bold text-gray-500">JOJO</p> 
+
+                    <p>sdtje</p>
+                </div>
+
+                <div className="w-[80%] ml-[20%] py-4 px-6 rounded-xl bg-blue-200">
+                    <p className="font-bold text-gray-500">coco</p> 
+
+                    <p>ssdfghjkltje</p>
+                </div>
+            </div>
+
+            <div className="mt-4 py-4 px-6 flex border border-gray-300 space-x-4 rounded-xl">
+                <input
+                    type="text"
+                    placeholder="Type your message here ..."
+                    className="w-full p-2 bg-gray-200 rounded-xl"
+                />
+
+                <CustomButton />
+            </div>
+        </>
     )
 }
 
