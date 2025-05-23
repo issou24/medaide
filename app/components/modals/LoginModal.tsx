4233df207285870628320ab8a902c0af52364563
LOFRZ <almenzo.arroyo@gmail.com>
2025-05-24 01:07:04 +0200

    log in fini

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -4,6 +4,7 @@ import Modal from "./Modal";
 
 import { useState } from "react";
 import useLoginModal from "@/app/hooks/useLoginModal";
+import CustomButton from "../forms/CustomButton";
 
 const LoginModal = () => {
     const loginModal = useLoginModal()
@@ -16,6 +17,16 @@ const LoginModal = () => {
             <input placeholder="E-mail address" type="email" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
 
             <input placeholder="Your password" type="password" className="w-full h-[54px] px-4 border border-gray-300 rounded-xl" />
+
+            <div className="p-5 bg-medaide text-white rounded-xl opacity-80">
+                Error
+            </div>
+
+            <CustomButton
+                label="Submit"
+                onClick={() => console.log("test")}
+            />
+
         </form>
         </>
     )
