8bd6e8ff032bfb0a7f76ec8f14bc1c85c4dd0572
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2025-07-31 18:13:19 +0200

    p

--- app/applications/[id]/page.tsx
+++ app/applications/[id]/page.tsx
@@ -2,49 +2,47 @@ import Image from "next/image";
 import ApplicationSidebar from "@/app/components/application/ApplicationSidebar";
 
 const ApplicationDetailPage = () => {
-    return (
-        <main className="maw-w-[1500px] mx-auto px-6 pb-6">
-            <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
-                <Image
-                    fill
-                    src='/beach_1.jpg'
-                    className="object-cover w-full h-full"
-                    alt="recherche medecine général"
-                />
-            </div>
+  return (
+    <main className="maw-w-[1500px] mx-auto px-6 pb-6">
+      <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+        <Image
+          fill
+          src="/medecinegenerale.png"
+          className="object-cover w-full h-full"
+          alt="recherche medecine générale"
+        />
+      </div>
 
-                <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
-                    <div className="py-6 pr-6 col-span-3">
-                        <h1 className="mb-4 text-4xl">Cand name</h1>
-                        <span className="mb-6 block text-lg text-gray-600">
-                                durée
-                        </span>
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-5 gap-4">
+        <div className="py-6 pr-6 col-span-3">
+          <h1 className="mb-4 text-4xl">Cand name</h1>
+          <span className="mb-6 block text-lg text-gray-600">durée</span>
 
-                        <hr />
+          <hr />
 
-                        <div className="py-6 flex items-center space-x-4">
-                            <Image
-                                src="/profile_pic_1.jpg"
-                                width={50}
-                                height={50}
-                                className="rounded-full"
-                                alt="The user name"
-                            />
+          <div className="py-6 flex items-center space-x-4">
+            <Image
+              src="/profile_pic_1.jpg"
+              width={50}
+              height={50}
+              className="rounded-full"
+              alt="The user name"
+            />
 
-                            <p><strong>Doc. Bunny</strong> recherche</p>
+            <p>
+              <strong>Doc. Bunny</strong> recherche
+            </p>
 
-                            <hr />
+            <hr />
 
-                            <p>je recherche pour blabla de temps a temps blabla</p>
-                        </div>
-                    </div>
+            <p>je recherche pour blabla de temps a temps blabla</p>
+          </div>
+        </div>
 
-                    
-                    <ApplicationSidebar />
-                    
-                </div>
-        </main>
-    )
-}
+        <ApplicationSidebar />
+      </div>
+    </main>
+  );
+};
 
 export default ApplicationDetailPage;
--- app/components/Categories.tsx
+++ app/components/Categories.tsx
@@ -1,109 +1,103 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 const Categories = () => {
-    return  (
-        <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
-            <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image
-                        src="/icn_category_beach.jpeg"
-                        alt="Category - Beach"
-                        width={20}
-                        height={20}
-                />
+  return (
+    <div className="pt-3 cursor-pointer pb-6 flex items-center space-x-12">
+      <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/medecinegenerale.png"
+          alt="Médecine Générale"
+          width={20}
+          height={20}
+        />
 
-                <span className='text-xs'>Médecine Général</span>
-            </div>
+        <span className="text-xs">Médecine Générale</span>
+      </div>
 
-            <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image
-                        src="/icn_category_beach.jpeg"
-                        alt="Category - Beach"
-                        width={20}
-                        height={20}
-                />
+      <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image src="/pediatrie.png" alt="Pédiatrie" width={20} height={20} />
 
-                <span className='text-xs'>Pédiatrie</span>
-            </div>
+        <span className="text-xs">Pédiatrie</span>
+      </div>
 
-            <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image
-                        src="/icn_category_beach.jpeg"
-                        alt="Category - Beach"
-                        width={20}
-                        height={20}
-                />
+      <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/gynécologie-obstétrique.png"
+          alt="Gynécologie-obstétrique"
+          width={20}
+          height={20}
+        />
 
-                <span className='text-xs'>Gynécologie-obstétrique</span>
-            </div>
+        <span className="text-xs">Gynécologie-obstétrique</span>
+      </div>
 
-            <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image
-                        src="/icn_category_beach.jpeg"
-                        alt="Category - Beach"
-                        width={20}
-                        height={20}
-                />
+      <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
 
-                <span className='text-xs'>Psychiatrie</span>
-            </div>
+        <span className="text-xs">Psychiatrie</span>
+      </div>
 
-            <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image
-                        src="/icn_category_beach.jpeg"
-                        alt="Category - Beach"
-                        width={20}
-                        height={20}
-                />
+      <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
 
-                <span className='text-xs'>Médecine d’urgence</span>
-            </div>
+        <span className="text-xs">Médecine d’urgence</span>
+      </div>
 
-            <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image
-                        src="/icn_category_beach.jpeg"
-                        alt="Category - Beach"
-                        width={20}
-                        height={20}
-                />
+      <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
 
-                <span className='text-xs'>Anesthésie-Réanimation</span>
-            </div>
+        <span className="text-xs">Anesthésie-Réanimation</span>
+      </div>
 
-            <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image
-                        src="/icn_category_beach.jpeg"
-                        alt="Category - Beach"
-                        width={20}
-                        height={20}
-                />
+      <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
 
-                <span className='text-xs'>Radiologie</span>
-            </div>
+        <span className="text-xs">Radiologie</span>
+      </div>
 
-            <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image
-                        src="/icn_category_beach.jpeg"
-                        alt="Category - Beach"
-                        width={20}
-                        height={20}
-                />
+      <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
 
-                <span className='text-xs'>Médecine du travail</span>
-            </div>
+        <span className="text-xs">Médecine du travail</span>
+      </div>
 
-            <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
-                <Image
-                        src="/icn_category_beach.jpeg"
-                        alt="Category - Beach"
-                        width={20}
-                        height={20}
-                />
+      <div className="pb-4 flex-col items-center space-y-2 border-b-2 opacity-60 hover:border-gray-200 hover:opacity-100">
+        <Image
+          src="/icn_category_beach.jpeg"
+          alt="Category - Beach"
+          width={20}
+          height={20}
+        />
 
-                <span className='text-xs'>Dermatologie</span>
-            </div>
-        </div>
-    )
-}
+        <span className="text-xs">Dermatologie</span>
+      </div>
+    </div>
+  );
+};
 
-
-export default Categories
+export default Categories;
